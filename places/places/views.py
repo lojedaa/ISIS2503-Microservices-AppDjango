d751a6e67faa7cf5942fc261f90d10ae7f06e1eb
lojedaa <l.ojedaa@uniandes.edu.co>
2024-05-21 22:30:30 -0500

    cambios

--- places/places/views.py
+++ places/places/views.py
@@ -1,4 +1,4 @@
-from .models import Variable
+from .models import Place
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import HttpResponse
@@ -7,7 +7,7 @@ from django.http import JsonResponse
 import json
 
 def PlaceList(request):
-    queryset = Variable.objects.all()
+    queryset = Place.objects.all()
     context = list(queryset.values('id', 'name'))
     return JsonResponse(context, safe=False)
 
@@ -15,7 +15,7 @@ def PlaceCreate(request):
     if request.method == 'POST':
         data = request.body.decode('utf-8')
         data_json = json.loads(data)
-        variable = Variable()
+        variable = Place()
         variable.name = data_json["name"]
         variable.save()
         return HttpResponse("successfully created place")
